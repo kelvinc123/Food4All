b8a563b66bc12c1b98ae78ccd73bdbe8b6038dcf
Kelvin Christian <kelvinchristian@Kelvins-MacBook-Pro.local>
2021-03-30 16:31:08 -0700

    add Yiyu's drivers page update

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:food4all_app/core/auth/authentication-service.dart';
-// import 'package:food4all_app/screens/feed.dart';
 import 'package:food4all_app/screens/loading.dart';
 import 'package:food4all_app/widgets/auth/authentication-wrapper.dart';
 import 'package:google_fonts/google_fonts.dart';
--- flutter/lib/screens/drivers.dart
+++ flutter/lib/screens/drivers.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+
+
 class Drivers extends StatefulWidget {
   @override
   _DriversState createState() => _DriversState();
@@ -9,55 +11,74 @@ class _DriversState extends State<Drivers> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Center(
-        child: Text("Available Pickup"),
-      )),
       body: DriverBody(),
     );
   }
 }
 
+
 class DriverBody extends StatelessWidget {
-  List<String> _testItems = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "h",
-    "i",
-    "j"
-  ];
+
+  DriverBody({Key key, this.title}) : super(key: key);
+
+  final String title;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView.builder(
-          scrollDirection: Axis.vertical,
-          itemCount: _testItems.length,
-          itemBuilder: (BuildContext context, int index) {
-            return RestaurantsWidget(_testItems[index]);
-          }),
-    );
+    return Scaffold(
+        appBar: AppBar(title: Text("Available Take-outs Nearby")),
+        body: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
+          children: <Widget>[
+            ProductBox(
+                name: "Shawama King",
+                description: "Available Take-outs: 10",
+                image: "shaw.png"),
+            ProductBox(
+                name: "Dough Zone",
+                description: "Available Take-outs: 10",
+                image: "dough.png"),
+            ProductBox(
+                name: "Valhalla Sandwiches",
+                description: "Available Take-outs: 10",
+                image: "val.png"),
+            ProductBox(
+                name: "Uneeda Burger",
+                description: "Available Take-outs: 10",
+                image: "une.png"),
+          ],
+        ));
   }
 }
 
-class RestaurantsWidget extends StatelessWidget {
-  final item;
 
-  RestaurantsWidget(this.item);
-
-  @override
+class ProductBox extends StatelessWidget {
+  ProductBox({Key key, this.name, this.description, this.image})
+      : super(key: key);
+  final String name;
+  final String description;
+  final String image;
   Widget build(BuildContext context) {
-    return Card(
-        child: ListTile(
-      title: Text(item),
-    ));
+    return Container(
+        padding: EdgeInsets.all(2),
+        height: 120,
+        child: Card(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+              Image.asset("assets/images/" + image),
+              Expanded(
+                  child: Container(
+                      padding: EdgeInsets.all(5),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Text(this.name,
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text(this.description),
+                        ],
+                      )))
+            ])));
   }
 }
